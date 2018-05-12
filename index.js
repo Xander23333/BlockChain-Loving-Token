7a4b76149dbfa08625a920f7950b7242e09bdbae
dixeran <dixeran@outlook.com>
2018-05-12 15:41:51 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- index.js
+++ index.js
@@ -6,51 +6,58 @@ var querystring = require('querystring');
 
 var http = require('http');
 var querystring = require('querystring');
-  
-var content = querystring.stringify(post_data);
 
-var options = {    
-  hostname: 'api-cn.faceplusplus.com',    
-  port: 443,    
-  path: '/facepp/v3/search',    
-  method: 'POST',    
-  headers: {    
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'    
-  }    
-}; 
 
-app.use(bodyParser.json());//use body-parser
-app.use(bodyParser.urlencoded());
+
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 app.post('/search', function (req, res) {
-  var img1_base64 = req.body.img1;
-  // post=api_key+api_secret+image_base64+face_set
-  var post_data = {    
-    api_key:'	bxry2NsPLpWdl-zL9SZgqBHqslXxbM1p',
-    api_secret:'XdJtVVbqEg4EzwOofqWRiWl_YiZSV_QT',
-    image_base64=img1_base64,
-    faceset_token:"4d05c631255dd4bc378586224ef58864"
-  };
-  //post!
-  var req = http.request(options, function (res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-    //JSON.parse(chunk)  
-    });
-  });  
-  req.on('error', function (e) {    
-      console.log('problem with request: ' + e.message);    
-  });
-  // write data to request body    
-  req.write(content);
-  req.end();
+    var img1_base64 = req.body.img1;
+    // post=api_key+api_secret+image_base64+face_set
+    var post_data = {
+
+    };
+    // var content = querystring.stringify(post_data);
+    // //post!
+    // var req = http.request(options, function (res) {
+    //     console.log('STATUS: ' + res.statusCode);
+    //     console.log('HEADERS: ' + JSON.stringify(res.headers));
+    //     res.setEncoding('utf8');
+    //     res.on('data', function (chunk) {
+    //         console.log('BODY: ' + chunk);
+    //         //JSON.parse(chunk)  
+    //     });
+    // });
+    // req.on('error', function (e) {
+    //     console.log('problem with request: ' + e.message);
+    // });
+    // // write data to request body    
+    // req.write(content);
+    // req.end();
+
+
+    var options = {
+        uri: 'https://api-cn.faceplusplus.com/facepp/v3/search',
+        method: 'POST',
+        form: {
+            api_key: 'bxry2NsPLpWdl-zL9SZgqBHqslXxbM1p',
+            api_secret: 'XdJtVVbqEg4EzwOofqWRiWl_YiZSV_QT',
+            image_base64: img1_base64,
+            faceset_token: "4d05c631255dd4bc378586224ef58864"
+        }
+    };
+    rq(options).then(result => {
+        console.log(result);
+    }).catch(err => {
+        console.log(result);
+    })
+
 })
 
 app.post('/insert', function (req, res) {
@@ -67,8 +74,7 @@ app.post('/insert', function (req, res) {
     var option = {
         uri: 'https://api-cn.faceplusplus.com/facepp/v3/search',
         method: 'POST',
-        body: content,
-        json: true
+        form: content
     }
     rq(option).then(result => {
 
