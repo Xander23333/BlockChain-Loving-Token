ea7fa12983d2b5c4b3072270e8d1fc97fefaf7f2
dixeran <dixeran@outlook.com>
2018-05-12 12:28:54 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());//use body-parser
+app.use(bodyParser.urlencoded());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post('/search', function (req, res) {
+    console.log(res.body.img1);
+})
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
