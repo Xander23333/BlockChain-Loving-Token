26014fd4e787d457a9fce1198ff11e97ead5afc7
dixeran <dixeran@outlook.com>
2018-05-12 14:37:51 +0800

    yjn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var rq = require('request-promise');
+var querystring = require('querystring');
 
 app.use(bodyParser.json());//use body-parser
 app.use(bodyParser.urlencoded());
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.post('/search', function (req, res) {
@@ -15,9 +17,33 @@ app.post('/search', function (req, res) {
     var rel = req.body.relation;
 })
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.post('/insert', function (req, res) {
+    var img1 = req.body.img1;
+    var img2 = req.body.img2;
+    var rel = req.body.rel;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    var content = querystring.stringify({
+        api_key: 'bxry2NsPLpWdl-zL9SZgqBHqslXxbM1p',
+        api_secret: 'XdJtVVbqEg4EzwOofqWRiWl_YiZSV_QT',
+        image_base64: img1,
+        faceset_token: '4d05c631255dd4bc378586224ef58864'
+    });
+    var option = {
+        uri: 'https://api-cn.faceplusplus.com/facepp/v3/search',
+        method: 'POST',
+        body: content,
+        json: true
+    }
+    rq(option).then(result => {
+
+    }).catch(err => {
+        console.log('face api error: ' + err);
+    });
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
 });
