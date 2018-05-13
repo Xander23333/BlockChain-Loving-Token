e5beb63a261f6b4290d090adf1e7f7a265560ba7
XanderXu1998 <xanderxu1998@gmail.com>
2018-05-13 09:48:11 +0800

    back v1.2

--- run/execute.py
+++ run/execute.py
@@ -5,7 +5,8 @@ account = '3ddba2981c42cecdfee9e845e5615ffc721f1daa'
 import curl_API as cl
 import json 
 
-begin = 1 # 截止块，截止后的区块链都是严格按照格式生成的可检索的块
+begin = 5 # 截止块，截止后的区块链都是严格按照格式生成的可检索的块
+username = 'lover_test' # 公用区块链的私有用户
 
 def init_acccount():
   resultjs = cl.invoke('addUser',
@@ -35,11 +36,14 @@ def search_relationship(user_id):
   result = json.loads(resultjs)
 
   for block in result:
+
     if block["id"]<begin:
       continue
-
+    
     transactions = block["transactions"][0]
-    if transactions["args"][0] == "addAsset":
+    if transactions["args"][-1]!=username:
+      continue
+    elif transactions["args"][0] == "addAsset":
       branch = 'add'
     elif transactions["args"][0] == "deleteAsset":
       branch = 'del'
@@ -50,7 +54,11 @@ def search_relationship(user_id):
     # "2018-05-12T15:37:14.000Z"
     when = ( datetime.strptime(when,"%Y-%m-%dT%H:%M:%S.000Z") + timedelta(hours = 8) ).strftime("%Y.%m.%d") 
     # "2018.5.12"
-    who1,who2,why = transactions["args"][1].split('&#@') 
+    try:
+      who1,who2,why = transactions["args"][1].split('&#@') 
+    except IndexError:
+      print(block)
+      raise(IndexError)
     if who1 == user_id:
       who = who2
     elif who2 == user_id:
@@ -61,4 +69,5 @@ def search_relationship(user_id):
     d.append( {"id":block["id"],"when":when,"who":who,"why":why,"type":branch} )
 
   d = sorted(d,key = lambda x:x["id"])
-  print(json.dumps(d, ensure_ascii=False ,indent=2))
+  answer = {"datas":d}
+  print(json.dumps(answer, ensure_ascii=False ,indent=2))
